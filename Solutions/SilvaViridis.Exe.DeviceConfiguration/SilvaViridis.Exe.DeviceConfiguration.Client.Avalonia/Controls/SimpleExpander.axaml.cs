2772f906f559f9a84eb3b0bcddf940693a1ae9ea
Maria Sorokina <ruafelianna@ya.ru>
2024-11-14 11:36:13 +0300

    Add SimpleExpander control; add menu and main content panels

---
+++ Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia/Controls/SimpleExpander.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia.Controls;
+
+public partial class SimpleExpander : Expander
+{
+    public SimpleExpander()
+    {
+        InitializeComponent();
+    }
+}

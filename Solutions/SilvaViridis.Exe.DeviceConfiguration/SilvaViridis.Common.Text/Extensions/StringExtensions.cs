c60157002355d288226d3c11d922a0e5aa5ca7ef
Maria Sorokina <ruafelianna@ya.ru>
2024-11-21 14:08:08 +0300

    Add string extensions methods: IsNullOrEmpty, IsNullOrWhitespace, TrimStart

---
+++ Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Common.Text/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace SilvaViridis.Common.Text.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(
+            [NotNullWhen(false)] this string? str
+        ) => string.IsNullOrEmpty(str);
+
+        public static bool IsNullOrWhiteSpace(
+            [NotNullWhen(false)] this string? str
+        ) => string.IsNullOrWhiteSpace(str);
+
+        public static string TrimStart(
+            this string str,
+            string start
+        ) => str.StartsWith(start)
+            ? str.Substring(start.Length)
+            : str;
+    }
+}

fe80347c951aa36865f398d8efbfdcd10cad32c5
Maria Sorokina <ruafelianna@ya.ru>
2024-11-21 15:11:12 +0300

    Add more System namespaces

--- Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Common.CSharp.Consts/Namespaces.cs
+++ Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Common.CSharp.Consts/Namespaces.cs
@@ -4,10 +4,263 @@ namespace SilvaViridis.Common.CSharp.Consts
     {
         public const string NS_System = nameof(System);
 
+        #region CodeDom
+
+        public const string NS_System_CodeDom = $"{NS_System}.{nameof(System.CodeDom)}";
+
+        public const string NS_System_CodeDom_Compiler = $"{NS_System_CodeDom}.{nameof(System.CodeDom.Compiler)}";
+
+        #endregion
+
+        #region Collections
+
         public const string NS_System_Collections
             = $"{NS_System}.{nameof(System.Collections)}";
 
+        public const string NS_System_Collections_Concurrent
+            = $"{NS_System_Collections}.{nameof(System.Collections.Concurrent)}";
+
         public const string NS_System_Collections_Generic
             = $"{NS_System_Collections}.{nameof(System.Collections.Generic)}";
+
+        public const string NS_System_Collections_ObjectModel
+            = $"{NS_System_Collections}.{nameof(System.Collections.ObjectModel)}";
+
+        public const string NS_System_Collections_Specialized
+            = $"{NS_System_Collections}.{nameof(System.Collections.Specialized)}";
+
+        #endregion
+
+        #region ComponentModel
+
+        public const string NS_System_ComponentModel = $"{NS_System}.{nameof(System.ComponentModel)}";
+
+        public const string NS_System_ComponentModel_Design = $"{NS_System_ComponentModel}.{nameof(System.ComponentModel.Design)}";
+
+        public const string NS_System_ComponentModel_Design_Serialization = $"{NS_System_ComponentModel_Design}.{nameof(System.ComponentModel.Design.Serialization)}";
+
+        #endregion
+
+        #region Configuration
+
+        public const string NS_System_Configuration = $"{NS_System}.{nameof(System.Configuration)}";
+
+        public const string NS_System_Configuration_Assemblies = $"{NS_System_Configuration}.{nameof(System.Configuration.Assemblies)}";
+
+        #endregion
+
+        #region Data
+
+        public const string NS_System_Data = $"{NS_System}.{nameof(System.Data)}";
+
+        public const string NS_System_Data_Common = $"{NS_System_Data}.{nameof(System.Data.Common)}";
+
+        public const string NS_System_Data_SqlTypes = $"{NS_System_Data}.{nameof(System.Data.SqlTypes)}";
+
+        #endregion
+
+        #region Diagnostics
+
+        public const string NS_System_Diagnostics = $"{NS_System}.{nameof(System.Diagnostics)}";
+
+        public const string NS_System_Diagnostics_CodeAnalysis = $"{NS_System_Diagnostics}.{nameof(System.Diagnostics.CodeAnalysis)}";
+
+        public const string NS_System_Diagnostics_Contracts = $"{NS_System_Diagnostics}.{nameof(System.Diagnostics.Contracts)}";
+
+        public const string NS_System_Diagnostics_SymbolStore = $"{NS_System_Diagnostics}.{nameof(System.Diagnostics.SymbolStore)}";
+
+        public const string NS_System_Diagnostics_Tracing = $"{NS_System_Diagnostics}.{nameof(System.Diagnostics.Tracing)}";
+
+        #endregion
+
+        #region Drawing
+
+        public const string NS_System_Drawing = $"{NS_System}.{nameof(System.Drawing)}";
+
+        #endregion
+
+        #region Dynamic
+
+        public const string NS_System_Dynamic = $"{NS_System}.{nameof(System.Dynamic)}";
+
+        #endregion
+
+        #region Globalization
+
+        public const string NS_System_Globalization = $"{NS_System}.{nameof(System.Globalization)}";
+
+        #endregion
+
+        #region IO
+
+        public const string NS_System_IO = $"{NS_System}.{nameof(System.IO)}";
+
+        public const string NS_System_IO_Compression = $"{NS_System_IO}.{nameof(System.IO.Compression)}";
+
+        public const string NS_System_IO_IsolatedStorage = $"{NS_System_IO}.{nameof(System.IO.IsolatedStorage)}";
+
+        public const string NS_System_IO_MemoryMappedFiles = $"{NS_System_IO}.{nameof(System.IO.MemoryMappedFiles)}";
+
+        public const string NS_System_IO_Pipes = $"{NS_System_IO}.{nameof(System.IO.Pipes)}";
+
+        #endregion
+
+        #region Linq
+
+        public const string NS_System_Linq = $"{NS_System}.{nameof(System.Linq)}";
+
+        public const string NS_System_Linq_Expressions = $"{NS_System_Linq}.{nameof(System.Linq.Expressions)}";
+
+        #endregion
+
+        #region Net
+
+        public const string NS_System_Net = $"{NS_System}.{nameof(System.Net)}";
+
+        public const string NS_System_Net_Cache = $"{NS_System_Net}.{nameof(System.Net.Cache)}";
+
+        public const string NS_System_Net_Http = $"{NS_System_Net}.{nameof(System.Net.Http)}";
+
+        public const string NS_System_Net_Http_Headers = $"{NS_System_Net_Http}.{nameof(System.Net.Http.Headers)}";
+
+        public const string NS_System_Net_Mail = $"{NS_System_Net}.{nameof(System.Net.Mail)}";
+
+        public const string NS_System_Net_Mime = $"{NS_System_Net}.{nameof(System.Net.Mime)}";
+
+        public const string NS_System_Net_NetworkInformation = $"{NS_System_Net}.{nameof(System.Net.NetworkInformation)}";
+
+        public const string NS_System_Net_Security = $"{NS_System_Net}.{nameof(System.Net.Security)}";
+
+        public const string NS_System_Net_Sockets = $"{NS_System_Net}.{nameof(System.Net.Sockets)}";
+
+        public const string NS_System_Net_WebSockets = $"{NS_System_Net}.{nameof(System.Net.WebSockets)}";
+
+        #endregion
+
+        #region Numerics
+
+        public const string NS_System_Numerics = $"{NS_System}.{nameof(System.Numerics)}";
+
+        #endregion
+
+        #region Reflection
+
+        public const string NS_System_Reflection = $"{NS_System}.{nameof(System.Reflection)}";
+
+        public const string NS_System_Reflection_Emit = $"{NS_System_Reflection}.{nameof(System.Reflection.Emit)}";
+
+        #endregion
+
+        #region Resources
+
+        public const string NS_System_Resources = $"{NS_System}.{nameof(System.Resources)}";
+
+        #endregion
+
+        #region Runtime
+
+        public const string NS_System_Runtime = $"{NS_System}.{nameof(System.Runtime)}";
+
+        public const string NS_System_Runtime_CompilerServices = $"{NS_System_Runtime}.{nameof(System.Runtime.CompilerServices)}";
+
+        public const string NS_System_Runtime_ConstrainedExecution = $"{NS_System_Runtime}.{nameof(System.Runtime.ConstrainedExecution)}";
+
+        public const string NS_System_Runtime_ExceptionServices = $"{NS_System_Runtime}.{nameof(System.Runtime.ExceptionServices)}";
+
+        public const string NS_System_Runtime_InteropServices = $"{NS_System_Runtime}.{nameof(System.Runtime.InteropServices)}";
+
+        public const string NS_System_Runtime_InteropServices_ComTypes = $"{NS_System_Runtime_InteropServices}.{nameof(System.Runtime.InteropServices.ComTypes)}";
+
+        public const string NS_System_Runtime_Serialization = $"{NS_System_Runtime}.{nameof(System.Runtime.Serialization)}";
+
+        public const string NS_System_Runtime_Serialization_Formatters = $"{NS_System_Runtime_Serialization}.{nameof(System.Runtime.Serialization.Formatters)}";
+
+        public const string NS_System_Runtime_Serialization_Formatters_Binary = $"{NS_System_Runtime_Serialization_Formatters}.{nameof(System.Runtime.Serialization.Formatters.Binary)}";
+
+        public const string NS_System_Runtime_Serialization_Json = $"{NS_System_Runtime_Serialization}.{nameof(System.Runtime.Serialization.Json)}";
+
+        public const string NS_System_Runtime_Versioning = $"{NS_System_Runtime}.{nameof(System.Runtime.Versioning)}";
+
+        #endregion
+
+        #region Security
+
+        public const string NS_System_Security = $"{NS_System}.{nameof(System.Security)}";
+
+        public const string NS_System_Security_Authentication = $"{NS_System_Security}.{nameof(System.Security.Authentication)}";
+
+        public const string NS_System_Security_Authentication_ExtendedProtection = $"{NS_System_Security_Authentication}.{nameof(System.Security.Authentication.ExtendedProtection)}";
+
+        public const string NS_System_Security_Claims = $"{NS_System_Security}.{nameof(System.Security.Claims)}";
+
+        public const string NS_System_Security_Cryptography = $"{NS_System_Security}.{nameof(System.Security.Cryptography)}";
+
+        public const string NS_System_Security_Cryptography_X509Certificates = $"{NS_System_Security_Cryptography}.{nameof(System.Security.Cryptography.X509Certificates)}";
+
+        public const string NS_System_Security_Permissions = $"{NS_System_Security}.{nameof(System.Security.Permissions)}";
+
+        public const string NS_System_Security_Principal = $"{NS_System_Security}.{nameof(System.Security.Principal)}";
+
+        #endregion
+
+        #region Text
+
+        public const string NS_System_Text = $"{NS_System}.{nameof(System.Text)}";
+
+        public const string NS_System_Text_RegularExpressions = $"{NS_System_Text}.{nameof(System.Text.RegularExpressions)}";
+
+        #endregion
+
+        #region Threading
+
+        public const string NS_System_Threading = $"{NS_System}.{nameof(System.Threading)}";
+
+        public const string NS_System_Threading_Tasks = $"{NS_System_Threading}.{nameof(System.Threading.Tasks)}";
+
+        #endregion
+
+        #region Timers
+
+        public const string NS_System_Timers = $"{NS_System}.{nameof(System.Timers)}";
+
+        #endregion
+
+        #region Transactions
+
+        public const string NS_System_Transactions = $"{NS_System}.{nameof(System.Transactions)}";
+
+        #endregion
+
+        #region Web
+
+        public const string NS_System_Web = $"{NS_System}.{nameof(System.Web)}";
+
+        #endregion
+
+        #region Windows
+
+        public const string NS_System_Windows = $"{NS_System}.{nameof(System.Windows)}";
+
+        public const string NS_System_Windows_Input = $"{NS_System_Windows}.{nameof(System.Windows.Input)}";
+
+        #endregion
+
+        #region Xml
+
+        public const string NS_System_Xml = $"{NS_System}.{nameof(System.Xml)}";
+
+        public const string NS_System_Xml_Linq = $"{NS_System_Xml}.{nameof(System.Xml.Linq)}";
+
+        public const string NS_System_Xml_Resolvers = $"{NS_System_Xml}.{nameof(System.Xml.Resolvers)}";
+
+        public const string NS_System_Xml_Schema = $"{NS_System_Xml}.{nameof(System.Xml.Schema)}";
+
+        public const string NS_System_Xml_Serialization = $"{NS_System_Xml}.{nameof(System.Xml.Serialization)}";
+
+        public const string NS_System_Xml_XPath = $"{NS_System_Xml}.{nameof(System.Xml.XPath)}";
+
+        public const string NS_System_Xml_Xsl = $"{NS_System_Xml}.{nameof(System.Xml.Xsl)}";
+
+        #endregion
     }
 }

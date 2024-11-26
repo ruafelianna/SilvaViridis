9bed6719721f788a790ff60d65b923f2632a4f78
Maria Sorokina <ruafelianna@ya.ru>
2024-11-26 14:25:10 +0300

    Add basic Create Batch view

--- Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia/ViewLocator.cs
+++ Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia/ViewLocator.cs
@@ -1,8 +1,10 @@
-using SilvaViridis.Exe.DeviceConfiguration.Client.ViewModels;
-using SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia.Views;
 using HanumanInstitute.MvvmDialogs.Avalonia;
-using SilvaViridis.Exe.DeviceConfiguration.Client.ViewModels.Dialogs;
+using SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia.Views;
 using SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia.Views.Dialogs;
+using SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia.Views.Interfaces;
+using SilvaViridis.Exe.DeviceConfiguration.Client.ViewModels;
+using SilvaViridis.Exe.DeviceConfiguration.Client.ViewModels.Dialogs;
+using SilvaViridis.Exe.DeviceConfiguration.Client.ViewModels.Interfaces;
 
 namespace SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia
 {
@@ -14,6 +16,7 @@ namespace SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia
             Register<ViewSettingsViewModel, ViewSettingsView>();
             Register<DialogErrorViewModel, DialogErrorView>();
             Register<OneActionViewModel, OneActionView>();
+            Register<CreateBatchViewModel, CreateBatchView>();
         }
     }
 }

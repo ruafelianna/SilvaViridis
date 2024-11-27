1ecc72037bcadc306c6e6e59e814353d0708c7f4
Maria Sorokina <ruafelianna@ya.ru>
2024-11-27 10:39:56 +0300

    Add stab view for batches list

---
+++ Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia/ViewModels/Interfaces/BatchListViewModelProxy.cs
@@ -0,0 +1,50 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
+using SilvaViridis.Common.Localization.Abstractions;
+using SilvaViridis.Components;
+using SilvaViridis.Exe.DeviceConfiguration.Client.Assets.Translations;
+using SilvaViridis.Exe.DeviceConfiguration.Client.ViewModels.Interfaces;
+using SilvaViridis.Exe.DeviceConfiguration.Client.ViewModels.Models;
+using System;
+
+namespace SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia.ViewModels.Interfaces
+{
+    public class BatchListViewModelProxy : ViewModelBase
+    {
+        public BatchListViewModelProxy(BatchListViewModel source)
+        {
+            Source = source;
+
+            TableSource = new(source.Batches)
+            {
+                Columns = {
+                    new TextColumn<BatchViewModel, long>(
+                        CreateColumnHeader(Strings.BatchesList_Table_Id),
+                        vm => vm.Id
+                    ),
+                    new TextColumn<BatchViewModel, string>(
+                        CreateColumnHeader(Strings.BatchesList_Table_Name),
+                        vm => vm.Name
+                    ),
+                    new TextColumn<BatchViewModel, DateTime>(
+                        CreateColumnHeader(Strings.BatchesList_Table_Created),
+                        vm => vm.LocalCreated
+                    ),
+                },
+            };
+        }
+
+        public BatchListViewModel Source { get; }
+
+        public FlatTreeDataGridSource<BatchViewModel> TableSource { get; }
+
+        private static TextBlock? CreateColumnHeader(
+            ITranslationUnit tUnit
+        ) => new()
+            {
+                [!TextBlock.TextProperty]
+                    = tUnit.ValueObservable.ToBinding()
+            };
+    }
+}
---
+++ Solutions/SilvaViridis.Exe.DeviceConfiguration/SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia/Views/Interfaces/BatchListView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SilvaViridis.Exe.DeviceConfiguration.Client.Avalonia.Views.Interfaces;
+
+public partial class BatchListView : UserControl
+{
+    public BatchListView()
+    {
+        InitializeComponent();
+    }
+}
